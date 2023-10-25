9a079486abc12e9355a237ac6ebab917fe746122
skatam <smaar.katam@gmail.com>
2023-10-25 12:49:43 +0800

    creating subnets

---
+++ subnets.tf
@@ -0,0 +1,18 @@
+resource "aws_subnet" "public-subnet" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "192.168.0.0/24"
+  availability_zone = "us-east-1"
+  map_public_ip_on_launch = "true"
+  tags={
+    Name="new-subnet"
+  }
+}
+resource "aws_subnet" "private-subnet" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "192.168.1.0/24"
+  availability_zone = "us-east-1"
+  map_public_ip_on_launch = "true"
+  tags={
+    Name="subnet-1"
+  }
+}
