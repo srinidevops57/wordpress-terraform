53f9e85527d87e7bfd048206d6c8092746ee1785
skatam <smaar.katam@gmail.com>
2023-10-25 13:23:18 +0800

    updating the ig id

--- route-table.tf
+++ route-table.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "r" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "aws_internet_gateway.gateway-1"
+    gateway_id = "aws_internet_gateway.IG-1.id"
   }
   tags = {
     name="routing-1"
