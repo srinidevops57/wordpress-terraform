dc68dd67473d7effadde9b7b72437699f348a281
skatam <smaar.katam@gmail.com>
2023-10-27 11:03:55 +0800

    updated the ami

--- create-server.tf
+++ create-server.tf
@@ -1,6 +1,5 @@
 data "aws_ami" "ami" {
-  most_recent = true
-  name_regex = "Centos-8-Devops-Practice"
+    name_regex = "Centos-8-Devops-Practice"
   owners = ["973714476881"]
 }
 variable "instances" {
